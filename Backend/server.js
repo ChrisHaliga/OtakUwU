00352b43e0425becc81c157a46a3c3b398289b1a
Aina <aina.phakhruddin@gmail.com>
2021-02-24 13:03:46 -0600

    fixed server error

--- Backend/server.js
+++ Backend/server.js
@@ -20,8 +20,3 @@ const connection = mongoose.connection;
 connection.once('open', () => {
   console.log("MongoDB database connection established successfully");
 })
-
-app.listen(port, () => {
-    console.log(`Server is running on port: ${port}`);
-});
-
