26fa9bf5c24191950f29ea540430e730290d7099
Aina <aina.phakhruddin@gmail.com>
2021-02-24 12:45:30 -0600

    added error messages to server

--- Backend/server.js
+++ Backend/server.js
@@ -1,7 +1,7 @@
 const express = require('express');
 const cors = require('cors');
 const mongoose = require('mongoose');
-
+const parser = require('body-parser');
 
 require('dotenv').config();
 
@@ -12,7 +12,10 @@ app.use(cors());
 app.use( express.json());
 
 const uri = process.env.ATLAS_URI;
-mongoose.connect(uri, { useNewUrlParser: true, useCreateIndex: true });
+mongoose.connect(uri, { useNewUrlParser: true, useCreateIndex: true, useUnifiedTopology: true })
+  .then(()=> app.listen(port, () => {console.log(`Server is running on port: ${port}`)}))
+  .catch((error) => console.log("Error: " + error.message));
+
 const connection = mongoose.connection;
 connection.once('open', () => {
   console.log("MongoDB database connection established successfully");
