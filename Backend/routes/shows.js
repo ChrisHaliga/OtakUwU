fd52ee3ae58a291a1fe146f96f5259ec804d2fdd
Aina <aina.phakhruddin@gmail.com>
2021-02-24 14:18:31 -0600

    created first routes for shows

--- Backend/routes/shows.js
+++ Backend/routes/shows.js
@@ -2,25 +2,28 @@ const router = require('express').Router();
 let Show = require('../models/show.model');
 
 router.route('/').get((req, res) => {
+    // res.send("Hello");
     Show.find()
     .then(shows => res.json(shows))
     .catch(err => res.status(400).json('Error: ' + err));
 });
 
-router.route('/add').post((req, res) => {
-    const title = "Hunter x Hunter";
-    const link = "";
-    const icon = "";
-    const newShow = new Show({
-        title,
-        link,
-        icon,
-    });
+//route to add shows 
+// router.route('/add').post((req, res) => {
+    //hardcoded variables (not to actually be used!)
+//     const title = "Hunter x Hunter";
+//     const link = "";
+//     const icon = "../testdata/IMG_5897.jpeg";
+//     const newShow = new Show({
+//         title,
+//         link,
+//         icon,
+//     });
 
-    newShow.save()
-    .then(() => res.json('Show added'))
-    .catch(err => res.status(400).json('Error: ' + err));
-});
+//     newShow.save()
+//     .then(() => res.json('Show added'))
+//     .catch(err => res.status(400).json('Error: ' + err));
+// });
 
 
 module.exports = router;
