f453b43efb230e9b70511550e5d4e37ca4a2649f
Chris <cehaliga@uwm.edu>
2021-02-26 19:24:10 -0600

    merging

--- Backend/models/platform.model.js
+++ Backend/models/platform.model.js
@@ -3,11 +3,7 @@ const Schema = mongoose.Schema;
 
 const PlatformSchema = new Schema({
 
-<<<<<<< HEAD
-    name: 
-=======
     websiteName: 
->>>>>>> 1502c044e05209b8f5540f6bd62d0504322b7203
     {
         type: String,
         required: true,
@@ -20,23 +16,14 @@ const PlatformSchema = new Schema({
         required: true,
         unique: true,
     },
-<<<<<<< HEAD
-
-    icon: // This is the path to an image located somewhere in a folder in backend
-=======
     icon: 
->>>>>>> 1502c044e05209b8f5540f6bd62d0504322b7203
     {
         type: String,
         required: true,
         unique: true,
     }
 
-<<<<<<< HEAD
-},{timestamps: true});
-=======
  },{timestamps: true});
->>>>>>> 1502c044e05209b8f5540f6bd62d0504322b7203
 
 const Platform = mongoose.model('Platform', PlatformSchema);
 module.exports = Platform;
