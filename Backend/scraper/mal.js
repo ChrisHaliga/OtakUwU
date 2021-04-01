220d202458acc12e1fa93b8eb1cc52cc07737fad
Chris <cehaliga@uwm.edu>
2021-04-01 11:57:42 -0500

    ridaa

--- Backend/scraper/mal.js
+++ Backend/scraper/mal.js
@@ -1,4 +1,4 @@
-const request = require('request');
+/* const request = require('request');
 const cheerio = require('cheerio');
 let Show = require('../models/show.model');
 
@@ -9,25 +9,33 @@ var customHeaderRequest = request.defaults({
     headers: {'User-Agent': 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/88.0.4324.182 Safari/537.36'}
 })
 
+function sleep(ms) {
+  return new Promise(resolve => setTimeout(resolve, ms));
+}
+
 let getimg = (shows)=> {
     if(shows.length < 1) {
         console.log("Finished Scraping My Anime List.");
         return;
     }
     if(!shows[0].icon || !shows[0].icon.includes("http")){
-        let title = shows[0].title;
-        let url = 'https://myanimelist.net/search/all?q=' + (title.split(" ").join("%20")) + '&cat=all';
-        customHeaderRequest.get(url, function(err, resp, body) {
-            if (!err && resp.statusCode==200) {
-                const $ = cheerio.load(body);          
-                const icon = $('article').find("img").attr("data-src");
-                if(icon){
-                  Show.updateOne({title:title},{icon:icon})
-                  .then(succ => console.log(`${title} updated.`))
-                  .catch(err => console.log(`Error occured updating '${shows[0].title}'${err}`));
-                }
-            }
-        });
+        async function connect(){
+          await sleep(5000);
+          let title = shows[0].title;
+          let url = 'https://myanimelist.net/search/all?q=' + (title.split(" ").join("%20")) + '&cat=all';
+          customHeaderRequest.get(url, function(err, resp, body) {
+              if (!err && resp.statusCode==200) {
+                  const $ = cheerio.load(body);          
+                  const icon = $('article').find("img").attr("data-src");
+                  if(icon){
+                    Show.updateOne({title:title},{icon:icon})
+                    .then(succ => console.log(`MAL: ${title} updated.`))
+                    .catch(err => console.log(`Error occured updating '${shows[0].title}'${err}`));
+                  }
+              }
+          });
+        }
+        connect();
     }
     
     getimg(shows.slice(1));
@@ -41,7 +49,7 @@ exports.scrape = () =>{
     })
     .catch(err => console.log(err));  
 
-}
+} */
    /* const $ = cheerio.load(body);
      links = $('.cardName');
     $(links).each(function(i, link){
