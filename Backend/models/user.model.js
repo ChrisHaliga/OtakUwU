8672cede5018b6c20c05a8415fa48897861f8dd6
Aina <aina.phakhruddin@gmail.com>
2021-04-06 21:35:40 -0500

    added linked accounts for later use - stretch goals

--- Backend/models/user.model.js
+++ Backend/models/user.model.js
@@ -46,7 +46,8 @@ const UserSchema = new Schema({
                     required: true,
                 },
                 password: {
-
+                    type: String,
+                    required: true,
                 }
 
             }),
