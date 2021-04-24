53c400613796c6206c44a20fb82cc05f2738a055
Aina <aina.phakhruddin@gmail.com>
2021-04-24 17:20:21 -0500

    removed unused

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -3,7 +3,6 @@ import React from 'react';
 import "bootstrap/dist/css/bootstrap.min.css";
 import { BrowserRouter as Router, Route} from "react-router-dom";
 import Homepage from "./components/homepage.component";
-import Col from 'react-bootstrap/Col';
 import './App.css';
 function App() {
   return (
