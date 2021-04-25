0dc3b9621086360c1c3b8f7fcd0e8667b229f077
Kate Do <do.kate12@gmail.com>
2021-04-25 12:48:00 -0500

    merge conflict changes

--- Backend/models/watchlist.model.js
+++ Backend/models/watchlist.model.js
@@ -2,13 +2,9 @@ const mongoose = require('mongoose');
 const Schema = mongoose.Schema;
 
 
-const WatchListSchema = new Schema({
- 
-    id: {
-        type:Integer,
-        required: true,
-        unique: true
-    },
+
+const WatchlistSchema = new Schema({
+
     title: {
         type:String,
         required: true
@@ -28,5 +24,5 @@ const WatchListSchema = new Schema({
 
 
 
-    const WatchList = mongoose.model('Watchlist',WatchListSchema);
-    module.exports = WatchList;
+    const Watchlist = mongoose.model('Watchlist', WatchlistSchema);
+    module.exports = Watchlist;
