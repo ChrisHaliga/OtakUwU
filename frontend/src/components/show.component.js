0966a14902870fb3df1af4706393b6c68c178446
Aina <aina.phakhruddin@gmail.com>
2021-04-28 09:25:38 -0500

    restore popover

--- frontend/src/components/show.component.js
+++ frontend/src/components/show.component.js
@@ -15,7 +15,54 @@ export default function Show( {chooseShow, show, all_platforms, isMiddle} ) {
   //   //changing this value does not change how the show looks even though this show is marked as middle
   // };
 
-  if(isMiddle == show.title)
+  const popover = (
+    <Popover>
+  
+      <div class="card watch-card scroll">
+  
+        <div class="watch-top">
+          <div class="watch-component-title mt-3">
+            <h5>WatchLists</h5>
+          </div>
+        </div>
+  
+        <div class="watch-showname mt-2">
+          <h6 class="show-title">Add "{show.title}" to ...</h6>
+  
+        </div>
+  
+        <input class="watch-form form-control mb-3" type="text" placeholder="New List"></input>
+  
+        <div class="playlists">
+          <div class="row">
+            <div class="col-3">
+              <div>
+                <button class="button"></button>
+              </div>
+  
+  
+  
+            </div>
+            <div class="col-9 mt-3">
+              <h6 class="listname">PlayList1</h6>
+            </div>
+  
+          </div>
+  
+  
+        </div>
+      </div>
+  
+    </Popover>
+  );
+  
+  const Example = () => (
+  <OverlayTrigger trigger="click" placement="right" overlay={popover}>
+  
+    <Button variant="success" style={{backgroundColor:"black",borderColor:"black"}}><img src={plus} style={{ width:"2rem",height:"2rem"}} /></Button>
+  </OverlayTrigger>
+  );
+if(isMiddle == show.title)
   {
   return (
   <div class=" main card container mt-3 mb-3 ">
@@ -41,7 +88,9 @@ export default function Show( {chooseShow, show, all_platforms, isMiddle} ) {
               }
 
             </div>
-
+            <div class="add col-3">
+              <Example />
+            </div>
           </div>
         </div>
       </div>
