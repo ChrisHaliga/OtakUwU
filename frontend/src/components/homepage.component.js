5b45d850da1a4868406b5b5e26985b65644c4465
IbtesamAhmed2 <imahmed@uwm.edu>
2021-04-29 23:18:08 -0500

    commit

--- frontend/src/components/homepage.component.js
+++ frontend/src/components/homepage.component.js
@@ -4,7 +4,7 @@ import otakuwu1 from'./otakuwu1.png';
 import "bootstrap/dist/css/bootstrap.min.css";
 import axios from "axios";
 import { Form, Button, FormGroup, FormControl, ControlLabel, Row ,Card ,ListGroup,ListGroupItem,Figure} from "react-bootstrap";
-// import { Form, Col, Row } from "react-bootstrap";
+/// import { Form, Col, Row } from "react-bootstrap";
 import Col from 'react-bootstrap/Col';
 
 import Show from './show.component';
